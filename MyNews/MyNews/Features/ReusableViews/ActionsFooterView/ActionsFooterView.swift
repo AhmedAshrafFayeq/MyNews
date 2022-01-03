adf6e4243804586b4c73dd0882ab19694ef734a2
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-01-03 11:16:00 +0200

    edit classes format

--- MyNews/MyNews/Features/ReusableViews/ActionsFooterView/ActionsFooterView.swift
+++ MyNews/MyNews/Features/ReusableViews/ActionsFooterView/ActionsFooterView.swift
@@ -11,84 +11,83 @@ import UIKit
 //
 class ActionsFooterView: UIView {
     
-  // MARK: - IBOutlets
+    // MARK: - IBOutlets
+    //
+    @IBOutlet var contentView: UIView!    
+    @IBOutlet weak var stackView: UIStackView!
     
-  @IBOutlet var contentView: UIView!
-  
-  @IBOutlet weak var stackView: UIStackView!
-  
-  // MARK: - Proeprties
-  
-  private(set) var actions: [ButtonAction] = []
-  
-  // MARK: - Init
-  
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    commitInit()
-    configureView()
-  }
-  
-  required init?(coder: NSCoder) {
-    super.init(coder: coder)
-    commitInit()
-    configureView()
-  }
+    // MARK: - Proeprties
+    //
+    private(set) var actions: [ButtonAction] = []
+    
+    // MARK: - Init
+    //
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commitInit()
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commitInit()
+        configureView()
+    }
 }
 
 // MARK: - ConfigureView
 //
 private extension ActionsFooterView {
-  
-  func commitInit() {
-    Bundle.main.loadNibNamed(ActionsFooterView.classNameWithoutNamespaces, owner: self, options: nil)
-    contentView.addAndFixInView(self)
-  }
-  
-  func configureView() {
-    configureContentView()
-  }
-  
-  func configureContentView() {
-    contentView.backgroundColor = .clear
-    backgroundColor = .clear
-  }
+    
+    func commitInit() {
+        Bundle.main.loadNibNamed(ActionsFooterView.classNameWithoutNamespaces, owner: self, options: nil)
+        contentView.addAndFixInView(self)
+    }
+    
+    func configureView() {
+        configureContentView()
+    }
+    
+    func configureContentView() {
+        contentView.backgroundColor = .clear
+        backgroundColor = .clear
+    }
 }
 
 // MARK: - Public Handlers
 //
 extension ActionsFooterView {
-  
-  func addAction(_ action: ButtonAction) {
-    let button = action.button
-    actions.append(action)
-    stackView.addArrangedSubview(button)
-  }
+    
+    func addAction(_ action: ButtonAction) {
+        let button = action.button
+        actions.append(action)
+        stackView.addArrangedSubview(button)
+    }
 }
 
 // MARK: - Loading Handlers
 //
 extension ActionsFooterView {
-  
-  /// Returns reference to loading buttons
-  ///
-  private var loadingButtons: [LoadingButton] {
-    return actions.compactMap { $0.button as? LoadingButton }
-  }
-  
-  /// Starts the loading buttons animation.
-  ///
-  func ensureLoadingIsStarted() {
-    loadingButtons.forEach { button in
-      button.showLoading()
+    
+    /// Returns reference to loading buttons
+    ///
+    private var loadingButtons: [LoadingButton] {
+        return actions.compactMap { $0.button as? LoadingButton }
     }
-  }
-  
-  /// Stops animating the Loading buttons.
-  ///
-  func ensureLoadingIsStopped() {
-    loadingButtons.forEach { button in
-      button.hideLoading()
+    
+    /// Starts the loading buttons animation.
+    ///
+    func ensureLoadingIsStarted() {
+        loadingButtons.forEach { button in
+            button.showLoading()
+        }
     }
-  }
+    
+    /// Stops animating the Loading buttons.
+    ///
+    func ensureLoadingIsStopped() {
+        loadingButtons.forEach { button in
+            button.hideLoading()
+        }
+    }
 }
